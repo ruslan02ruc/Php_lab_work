c9fe71dd132aba3572ece79e26f6689687130953
ruslan <ruslan02ruc@gmail.com>
2021-04-15 12:51:26 +0600

    ЛПР22

--- mysite.local/table.php
+++ mysite.local/table.php
@@ -1,3 +1,7 @@
+<?php
+$cols = 18;
+$rows = 15;
+?>
 <!DOCTYPE html>
 <html>
 
@@ -38,43 +42,21 @@
       <input type='submit' value='Создать' />
     </form>
     <!-- Таблица -->
-    <table border='1' width="200">
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>4</td>
-        <td>6</td>
-        <td>8</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>6</td>
-        <td>9</td>
-        <td>12</td>
-        <td>15</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>8</td>
-        <td>12</td>
-        <td>16</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>10</td>
-        <td>15</td>
-        <td>20</td>
-        <td>25</td>
-      </tr>
-    </table>
+    <?php
+    echo '<table border="1">';
+    for ($tr = 1; $tr <= $rows; $tr++) {
+      echo '<tr>';
+      for ($td = 1; $td <= $cols; $td++) {
+        if ($tr === 1 || $td === 1) {
+          echo '<th align="center" style="color:black;background-color:#07db83;">' . '<b>' . $tr * $td . '</b>' . '</th>';
+        } else {
+          echo '<td style="color:black;background-color:#7ecefcf4;">' . $tr * $td . '</td>';
+        }
+      }
+      echo '</tr>';
+    }
+    echo '</table>';
+    ?>
     <!-- Таблица -->
     <!-- Область основного контента -->
   </div>
